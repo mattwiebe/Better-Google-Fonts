8222fda5c63ad332130de5ebf2f574f9211fd098
Matt Wiebe <matt@somadesign.ca>
2013-01-13 16:45:01 -0600

    Move to a Backbone.js app
    This was a fun excuse to mess with Backbone.js, and will make
    filtering--a feature I've wanted here--feasible in the future.

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+uglifyjs underscore-min.js backbone-min.js google-families.js  -m -c -o app.js
--- google-families.js
+++ google-families.js
@@ -1,84 +1,86 @@
-jQuery(document).ready(function($) {
-	var target = $("#fonts"),
-		api = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBUK3PeqSEzwPNIyg94dBQpziFOPvm7-aA&sort=style',
-	gFontList = [],
-	onSuccess = function(data){
-		if ( data.kind === "webfonts#webfontList") {
-			$.each(data.items, function(index, value){
-				if ( value.variants.length > 1 ) {
-					gFontList.push(value);
-				}
-			})
-			renderFontList(gFontList)
-			grabFonts(gFontList)
+(function($){
+
+	// API URL with my own key. Please get your own if you want to use it.
+	var api = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBUK3PeqSEzwPNIyg94dBQpziFOPvm7-aA&sort=style';
+	// Will hold the font data returned by the API for future filtering operations
+	var fontData;
+	// Cached reference
+	var head = $("head");
+	// Minimum font variants to show a font
+	var minVariants = 2;
+	// Template for a single font
+	var template = _.template($("#font-template").html());
+	// Manually exclude these stupid fonts
+	var manualExcludes = ['Averia Libre', 'Averia Sans Libre', 'Averia Serif Libre'];
+
+	var Font = Backbone.Model.extend({
+		extUrlRoot: 'http://www.google.com/webfonts/specimen/',
+		// normalize our font data a bit
+		getFontData: function() {
+			var fontData = this.toJSON();
+			fontData.extlink = this.extUrlRoot + encodeURIComponent(fontData.family);
+			fontData.variants = _.map(fontData.variants, function(val){
+				return val.replace(/italic$/, ' italic');
+			});
+			return fontData;
 		}
-		else {
-			onError()
+	});
+
+	var FontView = Backbone.View.extend({
+		className: 'font',
+		template: template,
+		apiBase: 'http://fonts.googleapis.com/css?family=',
+		render: function() {
+			var fontData = this.model.getFontData();
+			this.getFont();
+			this.$el.html(this.template(fontData)).css({fontFamily: fontData.family});
+		},
+		getFont: function() {
+			var fontData = this.model.toJSON(),
+				tail = fontData.family + ':' + fontData.variants.join(',') + '&text=' + fontData.family + 'Iitalic1234567890',
+				url = this.apiBase + tail;
+			$('<link rel="stylesheet" href="'+url+'" >').appendTo(head);
 		}
-	},
-	onError = function() {
-		target.html("Error loading webfonts. :(")
+	});
+
+	var Fonts = Backbone.Collection.extend({
+		model: Font
+	});
+	var fonts = new Fonts();
+
+	var FontsView = Backbone.View.extend({
+		collection: fonts,
+		render: function() {
+			$("h1").prepend(this.collection.length + ' ');
+			this.collection.forEach(this.addOne, this);
+		},
+		addOne: function(font) {
+			var fontView = new FontView({model: font});
+			fontView.render();
+			this.$el.append(fontView.el);
+		}
+	});
+	var fontsView = new FontsView();
+
+	var renderFonts = function(data) {
+		if ( data.kind !== "webfonts#webfontList" )
+			return;
+
+		var items = _.filter(data.items, function(item){
+			return item.variants.length >= minVariants && manualExcludes.indexOf(item.family) == -1;
+		});
+		fontData = items;
+		fonts.reset(items);
+		fontsView.render();
+		$("#fonts").html(fontsView.el);
 	}
-	//*
-	$.ajax({
-		url: api,
-		type: "GET",
-		dataType: "jsonp",
-		success: onSuccess,
-		error: onError
-	})
-	//*/
-	function renderFontList(fonts) {
-		var template = "<div class='font' style='font-family:%name%'><h3><a href='%extlink%' target='_blank'>%name%</a></h3><ul>%variants%</ul></div>",
-			variantsTemplate = "<li style='%style%'>%variant%</li>",
-			variantsList = "",
-			specimenUrlPrefix = "http://www.google.com/webfonts/specimen/",
-			html = "",
-			fontData;
-		
-		$.each(fonts, function(i, val) {
-			variantsList = "";
-			$.each(val.variants, function(ii,vv){
-				variantsList += templatify(variantsTemplate, {variant: nicerVariant(vv), style: variantStyle(vv)})
-			})
-			html += templatify(template, {
-				'name': val.family,
-				'extlink': specimenUrlPrefix + encodeURIComponent(val.family),
-				'variants': variantsList
-			})
-		})
-		target.html(html)
-	}
-	
-	function templatify(html, data) {
-		var r
-		$.each(data, function(i,val){
-			r = new RegExp('%'+i+'%', 'g')
-			html = html.replace(r, val);
-		})
-		return html
-	}
-	
-	function nicerVariant(variant) {
-		return variant.replace('italic', ' italic')
-	}
-	
-	function variantStyle(variant) {
-		var text = variant.replace(/regular/i, '400').replace(/bold/i, '700'),
-			isItalic = /italic/.test(text),
-			weight = text.replace('italic', ''),
-			style = (weight !== '') ? 'font-weight:'+weight+';' : ''
-		return (isItalic) ? style + 'font-style:italic;'  : style
-	}
-	
-	function grabFonts(fontList) {
-		var base = "http://fonts.googleapis.com/css?family=",
-			families = [],
-			url, tail
-		$.each(fontList, function(i,v){
-			tail = v.family + ':' + v.variants.join(',')
-			$('<link rel="stylesheet" href="'+base+tail+'" >').appendTo("head")
-		})
-	}
-	
-});
+
+	jQuery(document).ready(function() {
+		$.ajax(api, {
+			dataType: 'jsonp',
+			jsonpCallback: 'callback',
+			success: renderFonts
+		});
+	});
+
+})(jQuery);
